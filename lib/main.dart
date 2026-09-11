06e16981db3eceaa8a72d6ac5ffca7f290078d7f
domeniksabitzer08-create <domeniksabitzer08@gmail.com>
2026-09-11 10:25:37 +0200

    added null saftey, animation and Bmi calculation, results are not displayed yet

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: .fromSeed(
-          seedColor: Colors.white,
+          seedColor: Colors.blueGrey,
           brightness: Brightness.dark,
         ),
       ),
--- lib/views/bmi_view.dart
+++ lib/views/bmi_view.dart
@@ -1,3 +1,5 @@
+import 'dart:developer' show log;
+
 import 'package:flutter/material.dart';
 
 class BmiView extends StatefulWidget {
@@ -8,37 +10,45 @@ class BmiView extends StatefulWidget {
 }
 
 class _BmiViewState extends State<BmiView> {
+  double? _bmi;
+  double? _height;
+  double? _weight;
+
+  bool _bmiValueIsNull = true;
+
+  void _animateHeadline() {
+    setState(() {
+      _bmiValueIsNull = !_bmiValueIsNull;
+    });
+  }
+
+  void _setHeight(double? height) {
+    _height = height ?? _height;
+    log(_height.toString());
+  }
+
+  void _setWeight(double? weight) {
+    _weight = weight ?? _weight;
+    log(_weight.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        //mainAxisAlignment: MainAxisAlignment.center,
-
         children: [
-          Padding(
-            padding: EdgeInsetsGeometry.directional(top: 120),
-            child: Align(
-              child: AppText(
-                data: "BMI",
-                fontSize: 100,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          AppText(
-            data: "BODY - MASS - INDEX",
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
+          AnimatedHeadline(isBig: _bmiValueIsNull),
           Padding(
             padding: EdgeInsetsGeometry.directional(top: 40),
             child: Row(
               children: [
                 AppInputField(
                   text: "Height",
+                  callback: _setHeight,
                 ),
                 AppInputField(
                   text: "Weight",
+                  callback: _setWeight,
                 ),
               ],
             ),
@@ -46,7 +56,15 @@ class _BmiViewState extends State<BmiView> {
           Padding(
             padding: EdgeInsetsGeometry.directional(top: 50),
             child: ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                if (_height != null && _weight != null) {
+                  setState(() {
+                    _bmi = _weight! / _height!;
+                    _bmiValueIsNull = false;
+                    _animateHeadline;
+                  });
+                }
+              },
               style: ButtonStyle(),
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -63,7 +81,44 @@ class _BmiViewState extends State<BmiView> {
   }
 }
 
-class AppText extends StatelessWidget {
+class AnimatedHeadline extends StatefulWidget {
+  final bool isBig;
+  const AnimatedHeadline({super.key, required this.isBig});
+
+  @override
+  State<AnimatedHeadline> createState() => _AnimatedHeadlineState();
+}
+
+class _AnimatedHeadlineState extends State<AnimatedHeadline> {
+  @override
+  Widget build(BuildContext context) {
+    widget.isBig;
+    return Column(
+      //mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AnimatedPadding(
+          duration: Duration(milliseconds: 600),
+          curve: Curves.fastOutSlowIn,
+          padding: EdgeInsetsGeometry.directional(top: widget.isBig ? 120 : 40),
+          child: Align(
+            child: AppText(
+              data: "BMI",
+              fontSize: widget.isBig ? 120 : 50,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        AppText(
+          data: "BODY - MASS - INDEX",
+          fontSize: widget.isBig ? 20 : 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ],
+    );
+  }
+}
+
+class AppText extends StatefulWidget {
   final String data;
   final double? fontSize;
   final Color? color;
@@ -78,13 +133,22 @@ class AppText extends StatelessWidget {
   });
 
   @override
+  State<AppText> createState() => _AppTextState();
+}
+
+class _AppTextState extends State<AppText> {
+  @override
   Widget build(BuildContext context) {
-    return Text(
-      data,
+    return AnimatedDefaultTextStyle(
+      duration: Duration(milliseconds: 600),
+      curve: Curves.fastOutSlowIn,
       style: TextStyle(
-        fontSize: fontSize,
-        color: color,
-        fontWeight: fontWeight,
+        fontSize: widget.fontSize,
+        color: widget.color,
+        fontWeight: widget.fontWeight,
+      ),
+      child: Text(
+        widget.data,
       ),
     );
   }
@@ -92,7 +156,8 @@ class AppText extends StatelessWidget {
 
 class AppInputField extends StatelessWidget {
   final String text;
-  const AppInputField({super.key, required this.text});
+  final Function(double? value) callback;
+  const AppInputField({super.key, required this.text, required this.callback});
 
   @override
   Widget build(BuildContext context) {
@@ -113,6 +178,7 @@ class AppInputField extends StatelessWidget {
                 horizontal: 10,
               ),
               child: TextField(
+                onChanged: (value) => callback(double.tryParse(value)),
                 decoration: InputDecoration(
                   border: InputBorder.none,
                 ),
